d35349c1f5ef273ca1db5c24145b01ced0545f85
Source <65256422+RealSourceOfficial@users.noreply.github.com>
2021-10-15 09:36:03 -0700

    Update LoginButton.js
    fixed redirect to wrong URL (Impatience)

--- Scripts/LoginButton.js
+++ Scripts/LoginButton.js
@@ -5,7 +5,7 @@ var errorDiv = document.getElementById("ErrorHandler")
 
 SubmitButton.addEventListener("click", function() {
     if (PWDinput.value === "abc123" && USRinput.value === "source") {
-        window.location.href = "../page.html"
+        window.location.href = "../Pages/page.html"
     } else {
         errorDiv.style.visibility = "visible"
         console.log(PWDinput.value + '' + USRinput.value)
